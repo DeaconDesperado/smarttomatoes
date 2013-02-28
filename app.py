a3efb5c778419087a7846463412b8d35a50837ec
Mark Grey <mark.grey@nytimes.com>
2013-02-28 17:57:52 -0500

    working pearson similarity

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from time import sleep
 from sets import Set
 from config import Config
 import minimongo
+from math import sqrt
 
 def mongo_config(config,prefix='MONGODB_'):
     attrs = config.__dict__.iteritems()
@@ -35,14 +36,41 @@ def most_prolific():
         for key in common:
             mapped[crit._id][key] = recs[key]
 
-    print mapped
+    return mapped
 
 
+def sim_pearson(person_a,person_b):
+    """
+    define the pearson similarity between two people
+    across mutliple dimensions
+    """
+    
+    #for this iteration, only consider common elements
+    n = len(person_a)
+
+    person_a_sum = sum(person_a.values())
+    person_b_sum = sum(person_b.values())
+
+    person_a_square = sum([pow(val,2) for val in person_a.values()])
+    person_b_square = sum([pow(val,2) for val in person_b.values()])
+    
+    pSum = sum([person_a[key]*person_b[key] for key in person_a.keys()])
+    num = pSum-(person_a_sum*person_b_sum/n)
+    density = sqrt((person_a_square-pow(person_a_sum,2)/n) * (person_b_square-pow(person_b_sum,2)/n))
+    if density == 0:
+        return 0
+    r = num/density
+    return r
+    
 
 def main():
     if not checkCache():
         buildCache()
         makeCritics()
-    most_prolific()
+    mapped = most_prolific()
+    for key in mapped:
+        others = [other_key for other_key in mapped.keys() if other_key != key]
+        for next in others:
+            print '%s->%s = %s' % (key,next,sim_pearson(mapped[key],mapped[next]))
 
 main()
